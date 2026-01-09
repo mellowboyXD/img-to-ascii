d106d48e8370d2cc404755658a6f6e2cbbc01105
mellowboyXD <chamerallyishfaq@gmail.com>
2026-01-08 21:57:05 -0500

    Convert input image to grayscale

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CC=gcc
 CFLAGS=-I./include/ -Wall -Wextra -g
+CLIBS=-lm
 
 all: build
 
 build: src/main.c include/stb_image.h
-	$(CC) $(CFLAGS) -o img-to-ascii src/main.c
+	$(CC) $(CFLAGS) $(CLIBS) -o bin/grayscale src/main.c
 
-clean: img-to-ascii
-	rm img-to-ascii
+clean: bin/
+	rm bin/*
--- src/main.c
+++ src/main.c
@@ -1,5 +1,62 @@
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-	printf("Hello World\n");
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+int main(int argc, char **argv)
+{
+	if (argc < 3) {
+		fprintf(stderr, "No image file provided\n");
+		exit(EXIT_FAILURE);
+	} else if (argc < 2) {
+		fprintf(stderr, "No output file name provided");
+		exit(EXIT_FAILURE);
+	}
+
+	const char *infile = argv[1];
+	const char *outfile = argv[2];
+
+	int width = 0;
+	int height = 0;
+	int channels = 0;
+	unsigned char *data = stbi_load(infile, &width, &height, &channels, 0);
+	if (data == NULL) {
+		const char *errmsg = stbi_failure_reason();
+		fprintf(stderr, "%s\n", errmsg);
+		exit(EXIT_FAILURE);
+	}
+
+	unsigned char *buf = (unsigned char *)malloc(width * height * 1);
+	if (buf == NULL) {
+		fprintf(stderr, "Enable to allocate memory.\n");
+		stbi_image_free(data);
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < height * width; ++i) {
+		int j = i * channels;
+		int r = data[j];
+		int g = data[j + 1];
+		int b = data[j + 2];
+
+		buf[i] = 0.299 * r + 0.587 * g + 0.114 * b;
+	}
+
+	if (stbi_write_png(outfile, width, height, 1, buf, 0) == 0) {
+		fprintf(stderr, "Error creating: %s\n", outfile);
+		stbi_image_free(data);
+		free(buf);
+		exit(EXIT_FAILURE);
+	}
+
+	stbi_image_free(data);
+	free(buf);
 }
