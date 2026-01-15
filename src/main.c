02183d7fc471794d42842d9719920c342cc7161b
mellowboyXD <chamerallyishfaq@gmail.com>
2026-01-15 16:29:02 -0500

    Revert "Parse output stream"
    This reverts commit 11ee944041801f48418f78b151e2ff449a421fce.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
-#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <getopt.h>
 
 #include "ascii.h"
 
@@ -9,61 +8,34 @@ int main(int argc, char **argv)
 {
 	char errmsg[1024] = "";
 	FILE *output = stdout;
-	int exit_status = 0;
-	bool file_output_flag = false;
 
 	if (argc < 2) {
 		fprintf(stderr, "No input file provided\n");
-		exit_status = 1;
-		goto main_close_output_file_and_exit;
+		return 1;
 	}
 
 	const char *infile = argv[1];
-	const char *outfile = NULL;
 
 	static struct option long_opt[] = {
-		{ "width", required_argument, 0, 'w' },
-		{ "output", required_argument, 0, 'o' },
-		{ 0, 0, 0, 0 }
+		{ "width", required_argument, 0, 'w' }, { 0, 0, 0, 0 }
 	};
 
 	int width = 80;
 	int c;
-	while ((c = getopt_long(argc, argv, "w:o:h", long_opt, &optind)) !=
-	       -1) {
+	while ((c = getopt_long(argc, argv, "w:h", long_opt, &optind)) != -1) {
 		switch (c) {
 		case 'w':
 			width = atoi(optarg);
 			break;
 
-		case 'o':
-			outfile = optarg;
-			output = fopen(outfile, "w");
-			if (!output) {
-				fprintf(stderr, "Error: could not open %s\n",
-					outfile);
-				exit_status = 1;
-				goto main_close_output_file_and_exit;
-			}
-			file_output_flag = true;
-			break;
-
 		default: /* '?' */
 			fprintf(stderr, "Usage: %s <image_file>\n", argv[0]);
-			goto main_close_output_file_and_exit;
+			return 1;
 		}
 	}
 
 	if (img2ascii(infile, width, output, errmsg) == 1) {
-		exit_status = 1;
+		return 1;
 	}
-
-	if (file_output_flag) {
-		fprintf(stdout, "Output file '%s' created\n", outfile);
-	}
-
-main_close_output_file_and_exit:
-	if (file_output_flag)
-		fclose(output);
-	return exit_status;
+	return 0;
 }
